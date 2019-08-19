f3752b4b49c5dca6944872c6433f48486562e667
Pensor <patrick.feldtmann@gmail.com>
2019-08-19 23:30:10 +0200

    Navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const mainNav = document.getElementById('menu');
 const navbarToggle = document.getElementById('navbar-toggle');
+const navbar = document.querySelector('.nav-bar');
 
 navbarToggle.addEventListener('click', () => {
   
   mainNav.classList.toggle('visible');
-
+  navbarToggle.classList.toggle('active');
+  navbar.classList.toggle('visible');
 });
