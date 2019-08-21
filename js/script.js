33d9800aa176ee676f77f8013d0aa23dcb8aa641
Pensor <patrick.feldtmann@gmail.com>
2019-08-21 12:28:58 +0200

    description slide in

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ navbarToggle.addEventListener('click', () => {
   navbarToggle.classList.toggle('active');
   navbar.classList.toggle('visible');
 });
+
+
+const myDescriptions = document.querySelectorAll(".show-on-scroll");
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('slide');
+    } //else {
+      //entry.target.classList.remove('slide');
+    //}
+  });
+});
+
+myDescriptions.forEach(description => {
+  observer.observe(description);
+})
