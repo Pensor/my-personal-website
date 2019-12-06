518656703d5f29ad25f26ad4fc425ad52a1d980e
Pensor <patrick.feldtmann@gmail.com>
2019-12-06 11:12:00 +0100

    edit slide animation

--- js/script.js
+++ js/script.js
@@ -3,25 +3,21 @@ const navbarToggle = document.getElementById('navbar-toggle');
 const navbar = document.querySelector('.nav-bar');
 
 navbarToggle.addEventListener('click', () => {
-  
-  mainNav.classList.toggle('visible');
-  navbarToggle.classList.toggle('active');
-  navbar.classList.toggle('visible');
+	mainNav.classList.toggle('visible');
+	navbarToggle.classList.toggle('active');
+	navbar.classList.toggle('visible');
 });
 
-
-const myDescriptions = document.querySelectorAll(".show-on-scroll");
+const myDescriptions = document.querySelectorAll('.show-on-scroll');
 
 observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add('slide');
-    } //else {
-      //entry.target.classList.remove('slide');
-    //}
-  });
+	entries.forEach(entry => {
+		if (entry.intersectionRatio > 0) {
+			entry.target.classList.add('slide');
+		}
+	});
 });
 
 myDescriptions.forEach(description => {
-  observer.observe(description);
-})
+	observer.observe(description);
+});
